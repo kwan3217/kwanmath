cea59c8aac2f38938d6c45e15321a74635a6c819
Chris Jeppesen <jeppesen@lasp.colorado.edu>
2021-09-17 20:38:16 -0600

    Updated README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# bezier
+Library for Bezier mathematics
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='bezier',
+    version='0.1.0',
+    description='Bezier mathematics',
+    url='https://github.com/kwan3217/bezier/',
+    author='kwan3217',
+    author_email='kwan3217@gmail.com',
+    license='BSD 2-clause',
+    packages=['bezier'],
+    install_requires=[
+                      'numpy',
+                     ],
+
+)
